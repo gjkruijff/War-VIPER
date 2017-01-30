7088380b86d5493a289de5f5f94169358229a1b0
Kruijff, Geert-Jan <gj.kruijff@gmail.com>
2017-01-30 09:00:05 +0100

    Documentation in ViewController

--- WarGame/ViewController.swift
+++ WarGame/ViewController.swift
@@ -12,13 +12,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var rightImageView: UIImageView!
     @IBOutlet weak var rightScoreLabel: UILabel!
+    /// The *variable* _rightScore_ is an Integer that keeps track of the CPU score (shown on the right of the screen)
     var rightScore : Int = 0
 
     @IBOutlet weak var leftImageView : UIImageView!
     @IBOutlet weak var leftScoreLabel: UILabel!
+    /// The *variable* _leftScore_ is an Integer that keeps track of the Player score (shown on the left of the screen)
     var leftScore : Int = 0
     
+    /// The *constant* _cardNames_ is an Array containing all fourteen (14) cardnames
     let cardNames = ["card2", "card3", "card4", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,14 +34,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /**
+    The *func* _dealTapped_ models the core Application Logic. Once the player taps the "deal" button, the function determines a random card for the player, another random card for the CPU, and then updates the score based on who won. 
+    
+    - Parameter sender: A clickable object (here: The button)
+    
+    - Todo: Need to refactor into Presenter and Interaction
 
+    */
     @IBAction func dealTapped(_ sender: Any) {
-        // Randomize left number in the range [0..13]
+        /** Randomize left number in the range [0..13] */
         let leftNumber = Int(arc4random_uniform(14))
+
         // Set the left image
         leftImageView.image = UIImage(named: cardNames[leftNumber])
-        // Randomize right number in the range [0..13]
+        
+        /** Randomize right number in the range [0..13] */
         let rightNumber = Int(arc4random_uniform(14))
+
+        // Set the right image 
         rightImageView.image = UIImage(named: cardNames[rightNumber])
         
         // Compare the card numbers
